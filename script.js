1cdc09f447b845eb972952a80faea1314f1a3b3f
Leo Barroso <leobarrosoadm@gmail.com>
2024-08-16 01:34:30 -0300

    selecionando campos do formulário

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
+
 amount.oninput = () => {
   let value = amount.value.replace(/\D/g, "")
   value = Number(value) / 100
@@ -11,4 +15,8 @@ function formatCurrencyBRL(value) {
     currency: "BRL"
   })
   return value
+}
+
+form.onsubmit = (event) => {
+  event.preventDefault()
 }
